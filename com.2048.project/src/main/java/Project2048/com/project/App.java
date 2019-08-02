6ff01e4ab62a10f013ef29eeba447e7a841a9d17
safin <safin@DESKTOP-158TJJU.homenet.telecomitalia.it>
2019-08-02 19:00:06 +0200

    First Commit

---
+++ com.2048.project/src/main/java/Project2048/com/project/App.java
@@ -0,0 +1,13 @@
+package Project2048.com.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
